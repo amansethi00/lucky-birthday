eb52fced0fe4d645e5442e292acd84a29ece74be
Aman Sethi <35420536+amansethi00@users.noreply.github.com>
2020-12-29 00:16:03 +0530

    visual updates

--- src/App.js
+++ src/App.js
@@ -42,47 +42,58 @@ export default function App() {
 
   return (
     <div className="App">
-      <h1>Lucky birthday App</h1>
-      <h2>Let's find out your are lucky or not</h2>
-      <h3>Enter your birthdate</h3>
-      <input type="date" id="birth-date" onChange={fetchBirthDate} />
-      <h3>Enter your lucky number</h3>
-      <input type="number" if="lucky-number" onChange={fetchLuckyNumber} />
-      <br />
-      <input type="submit" id="submit-btn" onClick={clickHandler} />
-      <Loader
-        loaded={loaded}
-        lines={13}
-        length={20}
-        width={10}
-        radius={30}
-        corners={1}
-        rotate={0}
-        direction={1}
-        color="#000"
-        speed={1}
-        trail={60}
-        shadow={false}
-        hwaccel={false}
-        className="spinner"
-        zIndex={2e9}
-        top="50%"
-        left="50%"
-        scale={1.0}
-        loadedClassName="loadedContent"
-      />
-      <h3>{output}</h3>
-      <footer className="footer card card-3">
+      <div className="container card-3">
+        <h1>Lucky birthday App</h1>
+        <h2>Let's find out your are lucky or not</h2>
+        <h3>Enter your birthdate</h3>
+        <input type="date" id="birth-date" onChange={fetchBirthDate} />
+        <h3>Enter your lucky number</h3>
+        <input type="number" id="lucky-number" onChange={fetchLuckyNumber} />
+        <br />
+        <input
+          type="submit"
+          id="submit-btn"
+          className="card-3"
+          onClick={clickHandler}
+        />
+        <Loader
+          loaded={loaded}
+          lines={13}
+          length={20}
+          width={10}
+          radius={30}
+          corners={1}
+          rotate={0}
+          direction={1}
+          color="#000"
+          speed={1}
+          trail={60}
+          shadow={false}
+          hwaccel={false}
+          className="spinner"
+          zIndex={2e9}
+          top="50%"
+          left="50%"
+          scale={1.0}
+          loadedClassName="loadedContent"
+        />
+        <h3>{output}</h3>
+      </div>
+      <footer className="footer card-3">
         <li>
-          <a href="https://github.com/amansethi00">Github</a>
+          <a style={{}} href="https://github.com/amansethi00">
+            Github
+          </a>
         </li>
         <li>
-          <a href="https://instagram.com/oye.sethi.00">Instagram</a>
-        </li>
-        <li>
-          <a href="https://twitter.com/amansethi00">Twitter</a>
-        </li>
-      </footer>
+          <a href="https://instagram.com/oye.sethi.00">
+            <img src="./assets/github."/></a>
+          </li>
+          <li>
+            <a href="https://twitter.com/amansethi00">Twitter</a>
+          </li>
+        </footer>
+      </div>
     </div>
   );
 }
